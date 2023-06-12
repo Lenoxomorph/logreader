aa50d2e5442b81defb3f4e186b1813f24cb50018
Lenoxomorph <github@lenoxc.com>
2023-06-12 12:15:35 -0400

    ErrorList

--- src/Tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
-import LogReader from '../LogReader/LogReader';
-
-test('renders learn react link', () => {
-    render(<LogReader/>);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
